b8a57c9f0b22440f074c949c844afd5c31b60944
Na90b1t <Na90b1t@mail.ru>
2022-04-30 23:11:54 +0300

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,14 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'ng-helloapp-metanit';
+  appTitle = 'ng-helloapp-metanit';
+  name = '';
+  surname = 'surname'
+
+  // log() {
+  //   console.log(12345);
+  // }
 }
