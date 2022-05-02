66d2d646f00d353eddf72109799de1e36a5e8943
Na90b1t <Na90b1t@mail.ru>
2022-05-02 05:25:47 +0300

    use @input and get and set

--- src/app/child-comp/child-comp.component.ts
+++ src/app/child-comp/child-comp.component.ts
@@ -7,7 +7,17 @@ import { Component, Input, OnInit } from '@angular/core';
 })
 export class ChildCompComponent implements OnInit {
   @Input() userName: string = '';
-  @Input() userAge: number = 0;
+  _userAge: number = 0;
+
+  @Input()
+  set userAge(age: number) {
+    if (age < 0) this._userAge = 0;
+    else if (age > 100) this._userAge = 100;
+    else this._userAge = age;
+  }
+  get userAge() {
+    return this._userAge;
+  }
 
   ngOnInit(): void {
     console.log('ChildCompComponent OnInit');
